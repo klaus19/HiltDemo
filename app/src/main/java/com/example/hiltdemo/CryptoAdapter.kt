1727ad3dbeb4cff6f04dd32d5321358d46fafb3d
tejaskhartude <tejaskhartude@gmail.com>
2022-06-12 09:28:31 +0530

    Updated the values and shown in recyclerview

--- app/src/main/java/com/example/hiltdemo/CryptoAdapter.kt
+++ app/src/main/java/com/example/hiltdemo/CryptoAdapter.kt
@@ -1,4 +1,50 @@
 package com.example.hiltdemo
 
-class CryptoAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.hiltdemo.data.Crypto
+
+class CryptoAdapter(private val crypto:List<Crypto>):RecyclerView.Adapter<CryptoAdapter.ViewHolder>() {
+
+
+
+
+    class ViewHolder(itemview: View) : RecyclerView.ViewHolder(itemview){
+
+        fun bind(index:Crypto){
+            // Here, we are using Glide library to
+            // load the image into ImageView
+
+            Glide.with(itemView.context)
+                .load(index.image).dontAnimate()
+                .into(itemView.findViewById(R.id.image))
+            // Setting name of cryptocurrency to TextView
+            itemView.findViewById<TextView>(R.id.cryptocurrency).text = index.name
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+         return crypto.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // Inflating list data from list_item to view
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_row, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        //Bind the inflated data
+        holder.bind(crypto[position])
+
+    }
+
+
 }
--- app/src/main/java/com/example/hiltdemo/MainActivity.kt
+++ app/src/main/java/com/example/hiltdemo/MainActivity.kt
@@ -2,14 +2,36 @@ package com.example.hiltdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hiltdemo.model.CryptoViewmodel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+private lateinit var cyptoList:RecyclerView
+
+// viewModels() delegate used to get
+// by view models will automatically construct the viewmodels using Hilt
+
+private val viewmodel:CryptoViewmodel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cyptoList = findViewById(R.id.cryptocurrency_list)
+        cyptoList.layoutManager = LinearLayoutManager(this)
+
+        observeCryptoCurrency()
+    }
+
+    // Observing the live data
+    private fun observeCryptoCurrency() {
+        viewmodel.crypto.observe(this) {
+              cyptoList.adapter = CryptoAdapter(it)
+        }
     }
 }
